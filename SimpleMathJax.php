2b085a5286ac9532afb6c653d6c84b14598e7156
jmnote <banking@nate.com>
2014-09-10 20:17:28 +0900

    New Repo

---
+++ SimpleMathJax.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * MediaWiki extension to render TeX
+ * Installation instructions can be found on
+ * http://www.mediawiki.org/wiki/Extension:SimpleMathJax
+ *
+ * @ingroup Extensions
+ * @author Jmkim dot com
+ * @license GNU Public License
+ */
+ 
+// Exit if called outside of MediaWiki
+if( !defined( 'MEDIAWIKI' ) ) exit;
+
+// Global Settings
+$wgSimpleMathJaxSize = 100;
+$wgExtensionCredits['parserhook'][] = array(
+        'path'        => __FILE__,
+        'name'        => 'SimpleMathJax',
+        'version'     => '0.1.0',
+        'author'      => 'Jmkim dot com',
+        'description' => 'render TeX between <nowiki><math></nowiki> and <nowiki></math></nowiki>',
+        'url'         => 'http://www.mediawiki.org/wiki/Extension:SimpleMathJax'
+);
+
+// Register class
+$dir = dirname(__FILE__) . '/';
+$wgAutoloadClasses['SimpleMathJax'] = $dir.'SimpleMathJax.class.php';
+
+$wgHooks['BeforePageDisplay'][] = 'SimpleMathJax::loadJS';
+$wgExtensionFunctions[] = 'SimpleMathJax::init';
