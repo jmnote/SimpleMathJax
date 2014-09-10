352f2e5637ce624025d45e559503e8670926eb12
jmnote <banking@nate.com>
2014-09-10 20:43:37 +0900

    gg

--- SimpleMathJax.php
+++ SimpleMathJax.php
@@ -23,9 +23,25 @@ $wgExtensionCredits['parserhook'][] = array(
         'url'         => 'http://www.mediawiki.org/wiki/Extension:SimpleMathJax'
 );
 
-// Register class
-$dir = dirname(__FILE__) . '/';
-$wgAutoloadClasses['SimpleMathJax'] = $dir.'SimpleMathJax.class.php';
-
 $wgHooks['BeforePageDisplay'][] = 'SimpleMathJax::loadJS';
 $wgExtensionFunctions[] = 'SimpleMathJax::init';
+
+class SimpleMathJax {
+	static function init() {
+	    global $wgParser;
+		$wgParser->setHook( 'math', 'SimpleMathJax::render' );
+	}
+
+	static function render($tex) {
+		return array("<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki');
+	}
+	
+	static function loadJS(&$out, &$skin ) {
+		global $wgSimpleMathJaxSize;
+		$out->addScript( "<style>.MathJax_Display{display:inline !important;}
+.mathjax-wrapper{font-size:${wgSimpleMathJaxSize}%;}</style>
+<script type='text/x-mathjax-config'>MathJax.Hub.Config({displayAlign:'left',tex2jax:{displayMath:[['[math]','[/math]']]}})</script>
+<script src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>" );
+		return true;
+	}
+}
