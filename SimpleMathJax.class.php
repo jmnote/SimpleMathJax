2605ffc3021a0d6a296b27e7c93c0a3b969911dc
jmnote <banking@nate.com>
2014-09-10 20:45:45 +0900

    Revert "gg"
    This reverts commit 352f2e5637ce624025d45e559503e8670926eb12.

---
+++ SimpleMathJax.class.php
@@ -0,0 +1,20 @@
+<?php
+class SimpleMathJax {
+	static function init() {
+	    global $wgParser;
+		$wgParser->setHook( 'math', 'SimpleMathJax::render' );
+	}
+
+	static function render($tex) {
+		return array("<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki');
+	}
+	
+	static function loadJS(&$out, &$skin ) {
+		global $wgSimpleMathJaxSize;
+		$out->addScript( "<style>.MathJax_Display{display:inline !important;}
+.mathjax-wrapper{font-size:${wgSimpleMathJaxSize}%;}</style>
+<script type='text/x-mathjax-config'>MathJax.Hub.Config({displayAlign:'left',tex2jax:{displayMath:[['[math]','[/math]']]}})</script>
+<script src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>" );
+		return true;
+	}
+}
