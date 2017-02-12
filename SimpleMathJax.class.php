924940b839015fed59f856a2e8a93221beee588c
jmnote <banking@nate.com>
2017-02-12 19:20:12 +0900

    mhchem.js works
    Now, mhchem.js on CDN works!

--- SimpleMathJax.class.php
+++ SimpleMathJax.class.php
@@ -11,42 +11,38 @@ class SimpleMathJax {
 
 	static function renderMath($tex) {
 		$tex = str_replace('\>', '\;', $tex);
-		$tex = str_replace('<', '\lt', $tex);
-		$tex = str_replace('>', '\gt', $tex);
-		return array("<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki');
+		$tex = str_replace('<', '\lt ', $tex);
+		$tex = str_replace('>', '\gt ', $tex);
+		return ["<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
 	}
 	
 	static function renderChem($tex) {
 		$tex = '\ce{'.$tex.'}';
-		return array("<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki');
+		return ["<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
 	}
 
 	static function loadJS(&$out, &$skin ) {
 		global $wgSimpleMathJaxSize, $wgSimpleMathJaxChem;
 
-		$config = array(
-			'displayAlign' => 'left',
-			'tex2jax' => array(
-				'displayMath' => array(
-					array(
-						'[math]','[/math]'
-					)
-				)
-			)
-		);
-
-		if( $wgSimpleMathJaxChem ) {
-			$config['extensions'] = array(
-				'[Contrib]/mhchem/mhchem.js'
-			);
-		}
-
+		$config = [
+			'messageStyle' => 'none',
+			'tex2jax' => [
+				'preview' => 'none',
+				'displayMath' => [ ['[math]','[/math]'] ],
+			]
+		];
 		$configJs = json_encode($config, JSON_UNESCAPED_SLASHES);
 
-		$out->addScript( "<style>.MathJax_Display{display:inline !important;}
-.mathjax-wrapper{font-size:${wgSimpleMathJaxSize}%;}</style>
-<script type='text/x-mathjax-config'>MathJax.Hub.Config(${configJs})</script>
-<script src='//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>" );
+		$script = <<<HEREDOC
+<style>.MathJax_Display{display:inline !important;}
+.mathjax-wrapper{display:none;font-size:${wgSimpleMathJaxSize}%;}</style>
+<script type='text/x-mathjax-config'>MathJax.Hub.Config(${configJs});MathJax.Hub.Queue(function(){\$('.mathjax-wrapper').show();});</script>
+<script src='//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
+HEREDOC;
+		if( $wgSimpleMathJaxChem ) {
+			$script .= "<script src='//cdn.mathjax.org/mathjax/latest/extensions/TeX/mhchem.js'></script>";
+		}
+		$out->addScript( $script );
 		return true;
 	}
 }
