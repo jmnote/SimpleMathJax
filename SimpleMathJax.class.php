adc17e8b1ba33e89fc587392db34fd4c6d882c61
James Montalvo <edwin.j.montalvo@nasa.gov>
2017-04-27 20:05:57 -0500

    Do not use CDNs due to security/privacy concerns

--- SimpleMathJax.class.php
+++ SimpleMathJax.class.php
@@ -15,7 +15,7 @@ class SimpleMathJax {
 		$tex = str_replace('>', '\gt ', $tex);
 		return ["<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
 	}
-	
+
 	static function renderChem($tex) {
 		$tex = '\ce{'.$tex.'}';
 		return ["<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
@@ -33,14 +33,21 @@ class SimpleMathJax {
 		];
 		$configJs = json_encode($config, JSON_UNESCAPED_SLASHES);
 
+		// create path to extension script files
+		global $wgServer, $wgExtensionAssetsPath;
+		$scriptBase = "$wgServer/$wgExtensionAssetsPath/SimpleMathJax/MathJax";
+
+		$mathjaxURL = "$scriptBase/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+
 		$script = <<<HEREDOC
 <style>.MathJax_Display{display:inline !important;}
 .mathjax-wrapper{display:none;font-size:${wgSimpleMathJaxSize}%;}</style>
 <script type='text/x-mathjax-config'>MathJax.Hub.Config(${configJs});MathJax.Hub.Queue(function(){\$('.mathjax-wrapper').show();});</script>
-<script src='//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
+<script src='$mathjaxURL'></script>
 HEREDOC;
 		if( $wgSimpleMathJaxChem ) {
-			$script .= "<script src='//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/extensions/TeX/mhchem.js'></script>";
+			$chemURL = "$scriptBase/extensions/TeX/mhchem.js";
+			$script .= "<script src='$chemURL'></script>";
 		}
 		$out->addScript( $script );
 		return true;
