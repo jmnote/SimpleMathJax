d62e3d1ca65524bdcbccf9673d72911e428d31c0
Jmnote <banking@nate.com>
2017-05-18 02:50:39 +0900

    version up and add authors

--- SimpleMathJax.php
+++ SimpleMathJax.php
@@ -8,7 +8,7 @@
  * @author Jmkim dot com
  * @license GNU Public License
  */
-
+ 
 // Exit if called outside of MediaWiki
 if( !defined( 'MEDIAWIKI' ) ) exit;
 
@@ -16,16 +16,17 @@ if( !defined( 'MEDIAWIKI' ) ) exit;
 $wgSimpleMathJaxSize = 100;
 $wgSimpleMathJaxChem = false;
 $wgExtensionCredits['parserhook'][] = array(
-        'path'        => __FILE__,
-        'name'        => 'SimpleMathJax',
-        'version'     => '0.3',
-        'author'      => 'Jmkim dot com',
-        'description' => 'render TeX between <nowiki><math></nowiki> and <nowiki></math></nowiki>',
-        'url'         => '//www.mediawiki.org/wiki/Extension:SimpleMathJax'
+	'path'        => __FILE__,
+	'name'        => 'SimpleMathJax',
+	'version'     => '0.5',
+	'author'      => 'jmnote, vedmaka, jamesmontalvo3, badshah400',
+	'description' => 'render TeX between <nowiki><math></nowiki> and <nowiki></math></nowiki>',
+	'url'         => '//www.mediawiki.org/wiki/Extension:SimpleMathJax'
 );
 
 // Register class
 $dir = dirname(__FILE__) . '/';
 $wgAutoloadClasses['SimpleMathJax'] = $dir.'SimpleMathJax.class.php';
 
-$wgHooks['ParserFirstCallInit'][] = 'SimpleMathJax::onParserFirstCallInit';
+$wgExtensionFunctions[] = 'SimpleMathJax::init';
+$wgHooks['BeforePageDisplay'][] = 'SimpleMathJax::addScripts';
