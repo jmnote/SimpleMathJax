96735e4a7a7330f989723f1615cccef424e2525c
Jmnote <banking@nate.com>
2017-05-18 03:00:17 +0900

    reverted some new ideas...
    'onParserFirstCallInit' is pretty good idea. But it makes math and chem tags disappear in 'Parser extension tags' on [[Special:Version]]
    'Lazy initialization of script' is also great idea. It shows unexpected result. I think because 'render' time and 'load' time are not same.

--- SimpleMathJax.class.php
+++ SimpleMathJax.class.php
@@ -1,65 +1,47 @@
 <?php
 class SimpleMathJax {
-
-	public static $mathjaxScriptLoaded = false;
-
-	static function onParserFirstCallInit ( Parser &$parser ) {
-		global $wgSimpleMathJaxChem;
-		$parser->setHook( 'math', 'SimpleMathJax::renderMath' );
-		if( $wgSimpleMathJaxChem ) {
-			$parser->setHook('chem', 'SimpleMathJax::renderChem');
-		}
+	
+	static function init() {
+		global $wgParser, $wgSimpleMathJaxChem;
+		$wgParser->setHook( 'math', 'SimpleMathJax::renderMath' );
+		if( $wgSimpleMathJaxChem )
+			$wgParser->setHook( 'chem', 'SimpleMathJax::renderChem' );
 	}
-
+	
 	static function renderMath($tex) {
-		if ( ! self::$mathjaxScriptLoaded ) {
-			self::$mathjaxScriptLoaded = true;
-			self::loadJS();
-		}
 		$tex = str_replace('\>', '\;', $tex);
 		$tex = str_replace('<', '\lt ', $tex);
 		$tex = str_replace('>', '\gt ', $tex);
-		return ["<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
+		return self::renderTex($tex);
 	}
-
+	
 	static function renderChem($tex) {
-		if ( ! self::$mathjaxScriptLoaded ) {
-			self::$mathjaxScriptLoaded = true;
-			self::loadJS();
-		}
 		$tex = '\ce{'.$tex.'}';
-		return ["<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
+		return self::renderTex($tex);
 	}
 
-	static function loadJS() {
-		global $wgOut, $wgSimpleMathJaxSize, $wgSimpleMathJaxChem;
-		$config = [
-			'messageStyle' => 'none',
-			'tex2jax' => [
-				'preview' => 'none',
-				'displayMath' => [ ['[math]','[/math]'] ],
-			]
-		];
-		$configJs = json_encode($config, JSON_UNESCAPED_SLASHES);
-
-		// create path to extension script files
-		global $wgServer, $wgExtensionAssetsPath;
-		$scriptBase = "$wgServer/$wgExtensionAssetsPath/SimpleMathJax/MathJax";
-
-		$mathjaxURL = "$scriptBase/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
-
-		$script = <<<HEREDOC
-<style>.MathJax_Display{display:inline !important;}
-.mathjax-wrapper{display:none;font-size:${wgSimpleMathJaxSize}%;}</style>
-<script type='text/x-mathjax-config'>MathJax.Hub.Config(${configJs});MathJax.Hub.Queue(function(){\$('.mathjax-wrapper').show();});</script>
-<script src='$mathjaxURL'></script>
-HEREDOC;
-		}
-		if( $wgSimpleMathJaxChem ) {
-			$chemURL = "$scriptBase/extensions/TeX/mhchem.js";
-			$script .= "<script src='$chemURL'></script>";
-		}
-		$wgOut->addScript( $script );
+	static function renderTex($tex) {
+		return ["<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
+	}
+	
+	static function addScripts( $out ) {
+		global $wgSimpleMathJaxSize, $wgSimpleMathJaxChem, $wgSimpleMathJaxMathJsUrlPath, $wgSimpleMathJaxChemJsUrlPath;
+		
+		if( !$wgSimpleMathJaxMathJsUrlPath )
+			$wgSimpleMathJaxMathJsUrlPath = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1';
+		if( !$wgSimpleMathJaxChemJsUrlPath )
+			$wgSimpleMathJaxChemJsUrlPath = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/extensions/TeX';
+		
+		$out->addScript( <<<HEREDOC
+<style>.mathjax-wrapper{display:none;font-size:${wgSimpleMathJaxSize}%;}.MathJax_Display{display:inline !important;}</style>
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({"messageStyle":"none","tex2jax":{"preview":"none","displayMath":[["[math]","[/math]"]]}});
+MathJax.Hub.Queue(function(){\$(".mathjax-wrapper").show();});</script>
+<script src="${wgSimpleMathJaxMathJsUrlPath}/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+HEREDOC
+);
+		if( $wgSimpleMathJaxChem )
+			$out->addScript( "<script src='${wgSimpleMathJaxChemJsUrlPath}/mhchem.js'></script>" );
 		return true;
 	}
 }
