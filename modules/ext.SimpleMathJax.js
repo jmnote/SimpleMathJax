82c6b5ce1ffe3e251bb9cef0e709f1161c8fe49b
Jmnote <banking@nate.com>
2017-08-15 19:00:14 +0900

    ensure mediawiki scripts loaded

--- modules/ext.SimpleMathJax.js
+++ modules/ext.SimpleMathJax.js
@@ -1,12 +1,12 @@
-MathJax.Ajax.config.root = mw.config.get('wgExtensionAssetsPath') + '/SimpleMathJax/modules/MathJax';
-MathJax.Hub.Config({
-	"messageStyle": "none",
-	"HTML-CSS": { scale: mw.config.get('wgSimpleMathJaxSize') },
-	"tex2jax": {
-		"preview": "none",
-		"inlineMath": mw.config.get('wgSimpleMathJaxInlineMath')
-	}
-});
-MathJax.Hub.Queue( function() {
-	$(".MathJax").parent().show();
+(window.RLQ=window.RLQ||[]).push(function() {
+	MathJax.Ajax.config.root = mw.config.get('wgExtensionAssetsPath') + '/SimpleMathJax/modules/MathJax';
+	MathJax.Hub.Config({
+		"messageStyle": "none",
+		"HTML-CSS": { scale: mw.config.get('wgSimpleMathJaxSize') },
+		"tex2jax": {
+			"preview": "none",
+			"inlineMath": mw.config.get('wgSimpleMathJaxInlineMath')
+		}
+	});
+	MathJax.Hub.Queue( function() { $(".MathJax").parent().show(); });
 });
