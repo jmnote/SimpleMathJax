795ae5d9f942ac592014dd86b3395680093f900d
jmnote <jmnote@users.noreply.github.com>
2017-10-22 02:03:25 +0900

    lazy loading module

--- SimpleMathJax_body.php
+++ SimpleMathJax_body.php
@@ -2,53 +2,41 @@
 class SimpleMathJax {
 
 	public static function onRegistration() {
-		global $wgSimpleMathJaxUseCDN, $wgExtensionFunctions, $wgSimpleMathJaxInlineMath;
-		$wgSimpleMathJaxInlineMath[] = ["[math]","[/math]"];
-		if( $wgSimpleMathJaxUseCDN ) $wgExtensionFunctions[] = __CLASS__ . '::setupCDN';
-		else $wgExtensionFunctions[] = __CLASS__ . '::setupLocal';
-		Hooks::register( 'ParserFirstCallInit', __CLASS__ . '::onParserFirstCallInit' );
+		Hooks::register( 'ParserFirstCallInit', __CLASS__ . '::setup' );
 	}
 
-	public static function setupCDN() {
-		global $wgOut, $wgSimpleMathJaxSize, $wgSimpleMathJaxInlineMath;
-		$inlineMath = json_encode($wgSimpleMathJaxInlineMath);
-		$wgOut->addScript( <<<HEREDOC
-<script type="text/x-mathjax-config">
-MathJax.Hub.Config({"messageStyle":"none","HTML-CSS":{scale:${wgSimpleMathJaxSize}},
-"tex2jax":{"preview":"none","inlineMath":${inlineMath}}});
-MathJax.Hub.Queue(function(){\$(".MathJax").parent().show();});</script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/extensions/TeX/mhchem.js"></script>
-HEREDOC
-);
-	}
+	public static function setup( Parser $parser ) {
+		global $wgOut, $wgSmjDisableChem, $wgSmjInlineMath, $wgSmjScripts, $wgSmjSize;
+		if( $wgSmjDisableChem != true ) $wgSmjDisableChem = false;
 
-	public static function setupLocal() {
-		global $wgOut, $wgSimpleMathJaxSize, $wgSimpleMathJaxInlineMath;
-		$wgOut->addJsConfigVars( 'wgSimpleMathJaxSize', $wgSimpleMathJaxSize );
-		$wgOut->addJsConfigVars( 'wgSimpleMathJaxInlineMath', $wgSimpleMathJaxInlineMath );
-		$wgOut->addModules( 'ext.SimpleMathJax' );
-	}
-
-	public static function onParserFirstCallInit( Parser $parser ) {
+		$wgSmjInlineMath[] = ["[math]","[/math]"];
+		if( !$wgSmjScripts ) {
+			$wgSmjScripts = ['//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS-MML_HTMLorMML'];
+			if( !$wgSmjDisableChem ) $wgSmjScripts[] = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/extensions/TeX/mhchem.js';
+		}
+		if( !$wgSmjSize ) $wgSmjSize = 110;
+		$wgOut->addJsConfigVars( 'wgSmjInlineMath', $wgSmjInlineMath );
+		$wgOut->addJsConfigVars( 'wgSmjScripts', $wgSmjScripts );
+		$wgOut->addJsConfigVars( 'wgSmjSize', $wgSmjSize );
+		
 		$parser->setHook( 'math', __CLASS__ . '::renderMath' );
-		$parser->setHook( 'chem', __CLASS__ . '::renderChem' );
+		if( !$wgSmjDisableChem ) $parser->setHook( 'chem', __CLASS__ . '::renderChem' );
 	}
 
-	public static function renderMath($tex) {
+	public static function renderMath($tex, array $args, Parser $parser, PPFrame $frame ) {
 		$tex = str_replace('\>', '\;', $tex);
 		$tex = str_replace('<', '\lt ', $tex);
 		$tex = str_replace('>', '\gt ', $tex);
-		return self::renderTex($tex);
-	}
-	
-	public static function renderChem($tex) {
-		$tex = '\ce{'.$tex.'}';
-		return self::renderTex($tex);
+		return self::renderTex($tex, $parser);
 	}
 
-	public static function renderTex($tex) {
+	public static function renderChem($tex, array $args, Parser $parser, PPFrame $frame ) {
+		$tex = '\ce{'.$tex.'}';
+		return self::renderTex($tex, $parser);
+	}
+
+	private static function renderTex($tex, $parser) {
+		$parser->getOutput()->addModules( 'ext.SimpleMathJax' );
 		return ["<span style='display:none'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
 	}
-
 }
--- modules/ext.SimpleMathJax.js
+++ modules/ext.SimpleMathJax.js
@@ -1,12 +1,22 @@
-MathJax.Ajax.config.root = mw.config.get('wgExtensionAssetsPath') + '/SimpleMathJax/modules/MathJax';
-MathJax.Hub.Config({
-	"messageStyle": "none",
-	"HTML-CSS": { scale: mw.config.get('wgSimpleMathJaxSize') },
-	"tex2jax": {
-		"preview": "none",
-		"inlineMath": mw.config.get('wgSimpleMathJaxInlineMath')
-	}
-});
-MathJax.Hub.Queue( function() {
-	$(".MathJax").parent().show();
+function loadScripts(scripts, index, callback) {
+	$.getScript(scripts[index], function () {
+		if(index + 1 <= scripts.length - 1) {
+			loadScripts(scripts, index + 1, callback);
+			return;
+		}
+		if(callback) callback();
+	});
+}
+loadScripts(mw.config.get('wgSmjScripts'), 0, function() {
+	MathJax.Hub.Config({
+		"messageStyle": "none",
+		"HTML-CSS": { scale: mw.config.get('wgSmjSize') },
+		"tex2jax": {
+			"preview": "none",
+			"inlineMath": mw.config.get('wgSmjInlineMath')
+		}
+	});
+	MathJax.Hub.Queue(function() {
+		$(".MathJax").parent().show();
+	});
 });
