a96d8c32416a9bc486998d3b3564342e1bf5a095
jmnote <jmnote@users.noreply.github.com>
2018-03-28 21:19:01 +0900

    dfrac bug fixed 2

--- modules/ext.SmjCDN.js
+++ modules/ext.SmjCDN.js
@@ -2,17 +2,17 @@ $.getScript( '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=T
 	function () {
 		var extensions = ["tex2jax.js"];
 		if( mw.config.get('wgSmjUseChem') ) {
-			window.MathJax.Ajax.config.path["mhchem"] = '//cdnjs.cloudflare.com/ajax/libs/mathjax-mhchem/3.3.0';
+			MathJax.Ajax.config.path["mhchem"] = '//cdnjs.cloudflare.com/ajax/libs/mathjax-mhchem/3.3.0';
 			extensions.push("[mhchem]/mhchem.js");
 		}
-		window.MathJax.Hub.Config({
+		MathJax.Hub.Config({
 			showMathMenu: false,
 			extensions: extensions,
 			jax: ["input/TeX", "output/HTML-CSS"],
 			"HTML-CSS": { scale: mw.config.get('wgSmjSize') },
 			tex2jax: { inlineMath: mw.config.get('wgSmjInlineMath') }
 		});
-		window.MathJax.Hub.Queue(function() { 
+		MathJax.Hub.Queue(function() { 
 			$(".MathJax").parent().css('opacity',1);
 		});
 	}
