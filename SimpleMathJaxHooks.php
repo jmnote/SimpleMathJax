85cf5ced71d20e41da80e115d6ffc2d5173aaa91
jmnote <opcore@gmail.com>
2020-10-23 19:25:34 +0900

    wgSmjWrapDisplaystyle

--- SimpleMathJaxHooks.php
+++ SimpleMathJaxHooks.php
@@ -1,33 +1,33 @@
 <?php
-class SimpleMathJax {
+class SimpleMathJaxHooks {
 
-	public static function onParserFirstCallInit( &$parser ) {
+	public static function onParserFirstCallInit( Parser $parser ) {
 		global $wgOut, $wgSmjUseCdn, $wgSmjUseChem, $wgSmjEnableMenu,
 			$wgSmjDisplayMath, $wgSmjExtraInlineMath,
 			$wgSmjScale, $wgSmjDisplayAlign;
 
 		$wgOut->addJsConfigVars( 'wgSmjUseCdn', $wgSmjUseCdn );
 		$wgOut->addJsConfigVars( 'wgSmjUseChem', $wgSmjUseChem );
-		$wgOut->addJsConfigVars( 'wgSmjEnableMenu', $wgSmjEnableMenu );
 		$wgOut->addJsConfigVars( 'wgSmjDisplayMath', $wgSmjDisplayMath );
 		$wgOut->addJsConfigVars( 'wgSmjExtraInlineMath', $wgSmjExtraInlineMath );
 		$wgOut->addJsConfigVars( 'wgSmjScale', $wgSmjScale );
+		$wgOut->addJsConfigVars( 'wgSmjEnableMenu', $wgSmjEnableMenu );
 		$wgOut->addJsConfigVars( 'wgSmjDisplayAlign', $wgSmjDisplayAlign );
 		
-		
 		$parser->setHook( 'math', __CLASS__ . '::renderMath' );
-		if( $wgSmjUseChem ) $parser->setHook( 'chem', __CLASS__ . '::renderChem' );
-	}
+		if( $wgSmjUseChem ) $parser->setHook( 'chem', __CLASS__ . '::renderChem' );	}
 	
 	public static function renderMath($tex, array $args, Parser $parser, PPFrame $frame ) {
+		global $wgSmjWrapDisplaystyle;
 		$tex = str_replace('\>', '\;', $tex);
 		$tex = str_replace('<', '\lt ', $tex);
 		$tex = str_replace('>', '\gt ', $tex);
+		if( $wgSmjWrapDisplaystyle ) $tex = "\displaystyle{ $tex }"; 
 		return self::renderTex($tex, $parser);
 	}
 
 	public static function renderChem($tex, array $args, Parser $parser, PPFrame $frame ) {
-		return self::renderTex('\ce{'.$tex.'}', $parser);
+		return self::renderTex("\ce{ $tex }", $parser);
 	}
 
 	private static function renderTex($tex, $parser) {
@@ -36,4 +36,8 @@ class SimpleMathJax {
 		return ["<span style='opacity:.5'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki'];
 	}
 }
+
+
+
+
 
